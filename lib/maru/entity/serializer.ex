1e2466c3829619ba44c5bb28b221a6fb94fa11b7
Teodor Pripoae <teodor.pripoae@gmail.com>
2015-03-10 08:01:42 +0200

    Serialize objects based on exposures

--- lib/maru/entity/serializer.ex
+++ lib/maru/entity/serializer.ex
@@ -2,7 +2,15 @@ defmodule Maru.Entity.Serializer do
   defmacro __using__(_) do
     quote do
       def serialize(record, options) when is_map(record) and is_map(options) do
-        %{}
+        Enum.reduce exposures, %{}, fn({as, opt}, acc) ->
+          attr_value = record[opt[:attr]]
+
+          if opt[:with] do
+            Map.put(acc, as, opt[:with].serialize(attr_value, options))
+          else
+            Map.put(acc, as, attr_value)
+          end
+        end
       end
 
       def serialize(records, options) when is_list(records) and is_map(options) do
--- test/maru/entity_test.exs
+++ test/maru/entity_test.exs
@@ -3,6 +3,24 @@ defmodule Maru.EntityTest do
 
   defmodule PostEntity do
     use Maru.Entity
+
+    expose :id
+    expose :title
+    expose :body, as: :content
+  end
+
+  defmodule CommentEntity do
+    use Maru.Entity
+
+    expose :body
+    expose :post, with: PostEntity
+  end
+
+  defmodule AuthorEntity do
+    use Maru.Entity
+
+    expose :name
+    expose :posts, with: PostEntity
   end
 
   describe "present" do
@@ -18,6 +36,26 @@ defmodule Maru.EntityTest do
                   %{id: 2, title: "My other title", content: "<b>html body</b>"}]
 
       assert PostEntity.serialize([post1, post2]) == expected
+    end
+
+    it "serializes stuff using with" do
+      post = %{id: 2, title: "My other title", body: "<b>html body</b>"}
+      comment = %{body: "<b>comment body</b>", post: post}
+      expected = %{body: "<b>comment body</b>", post: %{id: 2, title: "My other title", content: "<b>html body</b>"}}
+
+      assert CommentEntity.serialize(comment) == expected
+      assert CommentEntity.serialize([comment]) == [expected]
+    end
+
+    it "serializes array using with" do
+      post1 = %{id: 1, title: "My other title", body: "<b>html body</b>"}
+      post2 = %{id: 2, title: "My another title", body: "text body"}
+      author = %{name: "Teodor Pripoae", posts: [post1, post2]}
+      expected = %{name: "Teodor Pripoae", posts: [%{id: 1, title: "My other title", content: "<b>html body</b>"},
+                                                   %{id: 2, title: "My another title", content: "text body"}]}
+
+      assert AuthorEntity.serialize(author) == expected
+      assert AuthorEntity.serialize([author]) == [expected]
     end
   end
 end
