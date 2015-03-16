e50b50b51b6b85aad91bb715bbee29a004563466
Teodor Pripoae <teodor.pripoae@gmail.com>
2015-03-16 15:57:14 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MaruEntity.Mixfile do
 
   def project do
     [app: :maru_entity,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
      description: "Elixir copy of grape-entity",
      deps: deps]
