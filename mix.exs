b4a1da66cb0abaee7811a473fb7e16b83d3423b6
falood <falood@gmail.com>
2017-01-29 17:49:10 -0800

    update package infomation and license

--- mix.exs
+++ mix.exs
@@ -2,11 +2,21 @@ defmodule MaruEntity.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :maru_entity,
-     version: "0.1.2",
-     elixir: "~> 1.0",
-     description: "Elixir copy of grape-entity",
-     deps: deps()]
+    [ app: :maru_entity,
+      version: "0.1.2",
+      elixir: "~> 1.0",
+      description: "Elixir copy of grape-entity",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: "An API focused facade that sits on top of an object model inspired by grape-entity",
+      source_url: "https://github.com/elixir-maru/maru_entity",
+      package: package(),
+      docs: [
+        extras: ["README.md"],
+        main: "readme",
+      ]
+    ]
   end
 
   def application do
@@ -15,6 +25,14 @@ defmodule MaruEntity.Mixfile do
   end
 
   defp deps do
-    []
+    [ {:ex_doc, "~> 0.12", only: :docs},
+    ]
+  end
+
+  defp package do
+    %{ maintainers: ["Xiangrong Hao", "Teodor Pripoae"],
+       licenses: ["WTFPL"],
+       links: %{"Github" => "https://github.com/elixir-maru/maru_entity"}
+     }
   end
 end
